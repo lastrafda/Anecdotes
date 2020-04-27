72706a511f61b1c83eea5f3a38d3dc6b72edfa25
José Lastra <lastrafda@gmail.com>
2020-04-27 09:26:52 -0400

    show anecdote with most votes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const App = (props) => {
-	const initialPoints = Array(props.anecdotes.length).fill(0)
+const App = ({anecdotes}) => {
+	const initialPoints = Array(anecdotes.length).fill(0)
 	const [selected, setSelected] = useState(0)
 	const [points, setPoints] = useState(initialPoints)
 	const vote = () => {
@@ -10,12 +10,16 @@ const App = (props) => {
 		copy[selected] = copy[selected] + 1
 		setPoints(copy)
 	}
-	const getRandom = () => setSelected(Math.floor(Math.random() * props.anecdotes.length))
+	const getRandom = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+	const mostVoted = points.reduce((previousValue, currentValue, currentIndex, arr) => currentValue > arr[previousValue] ? currentIndex : previousValue, 0)
+	
 	return (
 	    <div>
-			<p>{props.anecdotes[selected]}</p>
+			<p>{anecdotes[selected]}</p>
 			<button onClick={vote}>Vote</button>
 			<button onClick={getRandom}>Get another one!</button>
+			<h2>Anecdote with most votes</h2>
+			<p>{anecdotes[mostVoted]}</p>
    		</div>
   	)
 }
