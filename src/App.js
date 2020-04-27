86aced8d1fe1cd702a496ad570b070cab9fcdef2
José Lastra <lastrafda@gmail.com>
2020-04-27 08:48:04 -0400

    used spread operator, and changed 'votes' to 'points'

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = (props) => {
-	const initialVotes = Array(props.anecdotes.length).fill(0)
+	const initialPoints = Array(props.anecdotes.length).fill(0)
 	const [selected, setSelected] = useState(0)
-	const [votes, setVotes] = useState(initialVotes)
+	const [points, setPoints] = useState(initialPoints)
 	const vote = () => {
-		const copy = Array.from(votes)
+		const copy = {...points}
 		copy[selected] = copy[selected] + 1
-		setVotes(copy)
+		setPoints(copy)
 	}
 	const getRandom = () => setSelected(Math.floor(Math.random() * props.anecdotes.length))
 	return (
