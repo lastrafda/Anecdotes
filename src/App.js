8c77c76c334f97b94e78ccb97318b27997c982e2
José Lastra <lastrafda@gmail.com>
2020-04-27 08:44:56 -0400

    Added votes state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = (props) => {
+	const initialVotes = Array(props.anecdotes.length).fill(0)
 	const [selected, setSelected] = useState(0)
-	const showAnecdote = () => setSelected(Math.floor(Math.random() * props.anecdotes.length))
-
+	const [votes, setVotes] = useState(initialVotes)
+	const vote = () => {
+		const copy = Array.from(votes)
+		copy[selected] = copy[selected] + 1
+		setVotes(copy)
+	}
+	const getRandom = () => setSelected(Math.floor(Math.random() * props.anecdotes.length))
 	return (
 	    <div>
 			<p>{props.anecdotes[selected]}</p>
-			<button onClick={showAnecdote}>Get another one!</button>
+			<button onClick={vote}>Vote</button>
+			<button onClick={getRandom}>Get another one!</button>
    		</div>
   	)
 }
